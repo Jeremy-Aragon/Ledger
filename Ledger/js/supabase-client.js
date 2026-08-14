857b8647e87f04dce7c23db6a1231daf089a114e
Jeremy Aragon <aragonjeremy143@gmail.com>
2026-08-14 18:59:34 +0800

    Update supabase-client.js

--- Ledger/js/supabase-client.js
+++ Ledger/js/supabase-client.js
@@ -6,7 +6,7 @@
 // 3. Paste them below
 // ============================================
 
-const SUPABASE_URL = 'YOUR_PROJECT_URL_HERE';       // e.g. https://orqgtbxcnzymdnwmcszc.supabase.co
+const SUPABASE_URL = 'https://orqgtxwcnzymdnwmcszc.supabase.co';       // e.g. https://orqgtxwcnzymdnwmcszc.supabase.co
 const SUPABASE_ANON_KEY = 'sb_publishable_nk9J1XwjnsfItqQ8XSk5Gw_sAY0G9in';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
